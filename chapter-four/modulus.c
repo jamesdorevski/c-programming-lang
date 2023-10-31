2396e62be10986cc6ad89dbe09d24ef9a57132bf
James Dorevski <github@jamesdorevski.com>
2023-11-01 07:57:22 +1100

    added modulus

---
+++ chapter-four/modulus.c
@@ -0,0 +1,130 @@
+/*
+
+Exercise 4-3. Given the basic framework, it's straightforward to extend the calculator. Add
+the modulus (%) operator and provisions for negative numbers.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAXOP 0
+#define NUMBER '0'
+#define MAXVAL 100
+#define BUFSIZE 100 /* char buffer size for ungetting */
+
+int getop(char c[]);
+void push(double v);
+double pop(void);
+int getch(void);
+void ungetch(int c);
+
+int sp = 0; /* stack pointer */
+double val[MAXVAL]; /* FIFO stack */
+
+int bufp = 0; /* buffer pointer */
+char buf[BUFSIZE]; /* buffer for ungetch */
+
+/* reverse Polish calculator */
+int main()
+{
+    int type; 
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '\n':
+                printf("error: unknown command %s\n", s);
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+/* push: push f onto value stack */
+void push(double f)
+{
+    if (sp < MAXVAL) {
+        val[sp++] = f;
+    } else {
+        printf("error: stack full, can't push %g\n", f);
+    }
+}
+
+/* pop: pop and return top value from stack */
+double pop()
+{
+    if (sp > 0) {
+        return val[--sp];
+    } else {
+        printf("error: stack empty\n");;
+        return 0.0;
+    }
+}
+
+/* getop: get next character or numeric operand */
+int getop(char s[])
+{
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t') {
+        ;
+    }
+
+    s[1] = '\0';
+
+    if (!isdigit(c)) {
+        while (isdigit(s[++i] = c = getch())) {
+            ;
+        }
+    }
+
+    if (c == '.') {
+        while (isdigit(s[++i] = c = getch())) {
+            ;
+        }
+    }
+
+    s[i] = '\0';
+
+    if (c != EOF) {
+        ungetch(c);
+    }
+
+    return NUMBER;
+}
+
+/* getch: gets next char from shared buffer. If empty, reads from getchar */
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* ungetch: places char onto buffer so it can potentially be read again. */
+void ungetch(int c)
+{
+    if (bufp < BUFSIZE) {
+        buf[bufp++] = c;
+    } else {
+        printf("error: char buffer full\n");
+    }
+}
